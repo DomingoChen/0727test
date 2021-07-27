bcee41c3131832b33e294dce0bf77d193bad91fd
DomingoChen <studentboy63@gmail.com>
2021-07-27 16:44:01 +0800

    small adjust

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import { useEffect, useContext } from 'react';
 import { dataContext } from './data.provider';
 
-const getJobList = async () => {
-  let url = 'http://localhost:5051/api/job';
-
-  let params = {
-    cache: 'no-cache',
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'GET'
-  };
-  return new Promise(async (resolve, reject) => {
-    try {
-      const data = await fetch(url, params);
-      const parsed = await data.json();
-      return resolve(parsed);
-    } catch (error) {
-      return reject(error);
-    }
-  });
-};
-
 function App() {
   const { handleSetData } = useContext(dataContext);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await getJobList();
-      handleSetData(response);
-    }
-    fetchData();
+    let url = 'http://localhost:5051/api/job';
+
+    let params = {
+      cache: 'no-cache',
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'GET'
+    };
+    fetch(url, params)
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        handleSetData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return <div className='App'></div>;
